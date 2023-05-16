9c4f538886c08dd33afb62ce5b57b61b75137464
Diego Vizcaino <diegovizcaino87@gmail.com>
2023-05-16 13:27:12 -0500

    added buttons with integrated links to the navbar

--- Components/navBar/navBar.js
+++ Components/navBar/navBar.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect} from "react";
 import styles from './styles/NavBar.module.css';
+import Link from 'next/link';
 
 
 function NavBar() {
     const[show, handleShow] = useState(false);
+    
 
     useEffect(() => {
 
@@ -21,22 +23,49 @@ function NavBar() {
 
     return (
         <div className={`${show && styles.nav__black} ${styles.nav}`}> 
-        
+        <nav className={styles.navbar}>
+      <div className={styles['navbar-logo']}>
             <img
                 className={styles.nav__logo}
                 src="http://watermann.wpengine.com/wp-content/uploads/2020/06/Watermann-Logo.png" 
                 alt="Watermann Logo"
                 />
-                
+                </div>
+    
+      <ul className={styles['navbar-list']}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/login">
+            <Button />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+        
+    <nav className={styles.navbar}>
+      <div className={styles['navbar-logo']}>
                 <img 
                 className={styles.nav__avatar}
                 src="http://watermann.wpengine.com/wp-content/uploads/2020/06/Watermann-Logo.png"
                 alt="Watermann Logo"
                 />
 
-                
+    </div>
+    </nav> 
         </div>
     )
 }
+
+function Button() {
+    return <button>Login</button>;
+  }
 
 export default NavBar;

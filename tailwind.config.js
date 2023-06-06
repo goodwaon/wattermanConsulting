ad08c17d57e1e4eac25117a598d998aed08562dd
Diego Vizcaino <diegovizcaino87@gmail.com>
2023-06-06 12:06:34 -0500

    deleted src line

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{html,js,ts,jsx,tsx}",
     "./components/**/*.{html,js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{html,js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {

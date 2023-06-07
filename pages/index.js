a2f604ef0151beaa243ab6449f866232a2e9b62c
Francisco <panchovizcaino@gmail.com>
2023-06-07 14:26:35 -0500

    changing config and next version

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+
 import styles from '/styles/Home.module.css'
 
 // Components
@@ -10,7 +10,7 @@ import Features from '../components/features'
 import Contact from '../components/contact'
 import About from '../components/about'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (

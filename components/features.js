b7b7837a7d49c88fe06fd3bfffc759b45dc8dcaf
Francisco <panchovizcaino@gmail.com>
2023-06-13 11:04:37 -0500

    fixing dark browser features

--- components/features.js
+++ components/features.js
@@ -5,7 +5,7 @@ function Features() {
 
     return (
         <div>
-            <section className="bg-grey bg-green-1050">
+            <section className="bg-green-1050">
                 <div className="container px-6 py-10 mx-auto">
                     <div className="lg:flex lg:items-center">
                         <div className="w-full space-y-12 lg:w-1/2 ">
@@ -20,7 +20,7 @@ function Features() {
                             </div>
 
                             <div className="md:flex md:items-start md:-mx-4">
-                                <span className="inline-block p-2 text-blue-500 bg-white rounded-xl md:mx-4 text-white ">
+                                <span className="inline-block p-2 text-blue-500 bg-white rounded-xl md:mx-4 ">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                                     </svg>
@@ -35,7 +35,7 @@ function Features() {
                             </div>
 
                             <div className="md:flex md:items-start md:-mx-4">
-                                <span className="inline-block p-2 text-blue-500 bg-white rounded-xl md:mx-4 text-white ">
+                                <span className="inline-block p-2 text-blue-500 bg-white rounded-xl md:mx-4 ">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
                                     </svg>
@@ -50,7 +50,7 @@ function Features() {
                             </div>
 
                             <div className="md:flex md:items-start md:-mx-4">
-                                <span className="inline-block p-2 text-blue-500 bg-white rounded-xl md:mx-4 text-white ">
+                                <span className="inline-block p-2 text-blue-500 bg-white rounded-xl md:mx-4 ">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
--- components/hero.js
+++ components/hero.js
@@ -4,12 +4,12 @@ function Hero() {
 
     return (
         <div>
-            <section className="bg-white dark:bg-gray-900">
+            <section className="bg-white">
                 <div className="container px-6 py-16 mx-auto text-center">
                     <div className="max-w-xl mx-auto">
-                        <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Employee Benefits and Consulting</h1>
-                        <p className="mt-6 text-gray-500 dark:text-gray-300">Watermann Consulting & Employee Benefits is built on two core principles</p> 
-                        <p className="text-gray-500 dark:text-gray-300"> – Professionalism and Integrity - </p>
+                        <h1 className="text-3xl font-semibold text-gray-800  lg:text-4xl">Employee Benefits and Consulting</h1>
+                        <p className="mt-6 text-gray-500 ">Watermann Consulting & Employee Benefits is built on two core principles</p> 
+                        <p className="text-gray-500 "> – Professionalism and Integrity - </p>
                         <button className="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-green-1050 rounded-lg hover:bg-green-950 lg:mx-0 lg:w-auto focus:outline-none">
                             Request A Free Quote Today
                         </button>

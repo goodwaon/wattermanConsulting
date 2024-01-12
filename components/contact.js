a69a775a7c6d4b6e2d1d236599fb17774dc5536c
Fransico Vizcaino <PanchoViz@GViz-MB-Pro-2019.local>
2024-01-12 09:27:58 -0600

    changed name for input

--- components/contact.js
+++ components/contact.js
@@ -21,27 +21,27 @@ function Contact() {
                     <div className="relative">
                       <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
                           absolute">First Name</p>
-                      <input placeholder="John" type="text" firstName="FirstName" className="border placeholder-gray-400 focus:outline-none
+                      <input placeholder="John" type="text" name="FirstName" className="border placeholder-gray-400 focus:outline-none
                           focus:border-black focus:ring-1 focus:ring-green-950 w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                           border-gray-300 rounded-md"/>
                     </div>
                     <div className="relative">
                       <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
                           absolute">Last Name</p>
-                      <input placeholder="Smith" type="text" lastName="LastName"className="border placeholder-gray-400 focus:outline-none
+                      <input placeholder="Smith" type="text" name="LastName"className="border placeholder-gray-400 focus:outline-none
                           focus:border-black focus:ring-1 focus:ring-green-950 w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                           border-gray-300 rounded-md"/>
                     </div>
                     <div className="relative">
                       <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">Email</p>
-                      <input placeholder="123@ex.com" type="email" className="border placeholder-gray-400 focus:outline-none
+                      <input placeholder="123@ex.com" type="email" name="email" className="border placeholder-gray-400 focus:outline-none
                           focus:border-black focus:ring-1 focus:ring-green-950 w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                           border-gray-300 rounded-md"/>
                     </div>
                     <div className="relative">
                       <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
                           absolute">Message</p>
-                      <input placeholder="Looking for a quote?" type="text" className="border placeholder-gray-400 focus:outline-none
+                      <input placeholder="Looking for a quote?" type="text" name="info" className="border placeholder-gray-400 focus:outline-none
                           focus:border-black focus:ring-1 focus:ring-green-950  w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                           border-gray-300 rounded-md"/>
                     </div>
